3f836b381484320a55600f06f2124a277eba9940
linyerui <2320314752@qq.com>
2020-02-22 09:50:04 +0800

    项目搭建

---
+++ src/main/java/com/sise/yizhan/YizhanServiceApplication.java
@@ -0,0 +1,13 @@
+package com.sise.yizhan;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class YizhanServiceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(YizhanServiceApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/sise/yizhan/YizhanServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sise.yizhan;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class YizhanServiceApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
